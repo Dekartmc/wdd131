5ebc44d1e134f95a0d1c167554c9985326aa066e
Michael Flores <michaeljorfs@gmail.com>
2025-07-13 04:03:31 -0600

    W01_Update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
-// Get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright").textContent = `© ${currentYear} | Michael Flores`;
 
-// Get the last modified date
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
