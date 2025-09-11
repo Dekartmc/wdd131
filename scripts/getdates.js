8c423f5d6d837d4dbbcedc2fdb1a51436c85714e
Michael Flores <michaeljorfs@gmail.com>
2025-09-11 01:46:35 -0600

    Term 5

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,2 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("copyright").textContent = `© ${currentYear} | Michael Flores`;
-
-const lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
